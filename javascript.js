48f81304b05cf8728249cf636255f4a43c62af52
James Martin <117032706+Jamtin68@users.noreply.github.com>
2022-11-15 15:48:57 +0000

    fixed getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,15 +1,20 @@
 function getComputerChoice() {
     const randNum = Math.floor(Math.random() * 3) + 1;
-    if (randNum == 1) {
-        const computerChoice = Rock;
-    } else if (randNum == 2) {
-        const computerChoice = Paper;
-    } else if (randNum == 3) {
-        const computerChoice = Scissor;
+    console.log(randNum)
+    
+    if (randNum === 1) {
+        computerChoice = "Rock";
+    } else if (randNum === 2) {
+        computerChoice = "Paper";
+    } else if (randNum === 3) {
+        computerChoice = "Scissor";
     } else {
-        const computerChoice = ERROR;
+        computerChoice = "ERROR";
     }
-    console.log(`The computer has picked ${computerChoice}`);
-}
 
+    return computerChoice;
+
+}
+let computerChoice;
 getComputerChoice();
+console.log("The computer has picked " + computerChoice);
